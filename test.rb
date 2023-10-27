653152fda8e880f097cd3b76275f2cfd929db851
“minhnguyennn” <minh8442657913@gmail.com>
2023-10-27 17:47:51 +0700

    Report 27/10

--- test.rb
+++ test.rb
@@ -1,11 +1,31 @@
-require 'test/unit'
-include Test::Unit::Assertions
+# require 'test/unit'
+# include Test::Unit::Assertions
 
-def your_method(array)
-  return false if !array.is_a?(Array) || array.size != 2 || array.any? { |element| !element.is_a?(String) || element.empty? }
+# def your_method(array)
+#   return false if !array.is_a?(Array) || array.size != 2 || array.any? { |element| !element.is_a?(String) || element.empty? }
 
-  fisrt_string, last_string = array
-  last_string.chars.uniq.all? { |char| fisrt_string.include?(char) }
+#   fisrt_string, last_string = array
+#   last_string.chars.uniq.all? { |char| fisrt_string.include?(char) }
+# end
+
+# Test::Unit::Assertions.assert_equal(your_method(["deltas", "slated"]), true)
+
+module Speak
+  def speak(sound)
+    puts "#{sound}"
+  end
 end
 
-Test::Unit::Assertions.assert_equal(your_method(["deltas", "slated"]), true)
+class GoodDog
+  include Speak
+end
+
+class HumanBeing
+  include Speak
+end
+
+puts "---GoodDog ancestors---"
+puts GoodDog.ancestors
+puts ''
+puts "---HumanBeing ancestors---"
+puts HumanBeing.ancestors
