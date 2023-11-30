8da39decc483cdf3affe3e31e1dd0c2f38545645
minhnguyennn <minh842657913@gmail.com>
2023-11-30 23:09:44 +0700

    Finish rspec for update and destroy

--- Week_7/30_11/Demo 10/spec/requests/employees_spec.rb
+++ Week_7/30_11/Demo 10/spec/requests/employees_spec.rb
@@ -1,39 +1,65 @@
-# employees_controller_spec.rb
 require 'rails_helper'
 
 RSpec.describe EmployeesController, type: :controller do
   let(:valid_attributes) { attributes_for(:employee) }
   let(:invalid_attributes) { attributes_for(:employee, employee_name: nil) }
+  let(:employee) { create(:employee) }
 
   describe 'PATCH #update' do
-    let(:employee) { create(:employee) }
-
     context 'with valid parameters' do
-      it 'updates the employee' do
+      before do
         patch :update, params: { id: employee.id, employee: valid_attributes }
         employee.reload
+      end
 
+      it 'updates the employee' do
         expect(employee.employee_name).to eq(valid_attributes[:employee_name])
       end
 
       it 'redirects to the employee show page' do
-        patch :update, params: { id: employee.id, employee: valid_attributes }
         expect(response).to redirect_to(employee_path(employee))
       end
     end
 
     context 'with invalid parameters' do
-      it 'does not update the employee' do
+      before do
         patch :update, params: { id: employee.id, employee: invalid_attributes }
         employee.reload
+      end
 
-        expect(employee.employee_name).to (invalid_attributes[:employee_name])
+      it 'does not update the employee' do
+        expect(employee.employee_name).not_to eq(invalid_attributes[:employee_name])
       end
 
       it 'renders the edit template' do
-        patch :update, params: { id: employee.id, employee: invalid_attributes }
-        expect(response).to render_template(:edit)
+        expect(response).to have_http_status(:redirect)
+        expect(response.body).to have_css("a[href='#{response.header['Location']}']")
+        expect(response).to redirect_to(edit_employee_path(employee))
       end
+    end
+  end
+
+  describe 'POST #destroy' do
+    context 'with valid parameters' do
+      before do
+        post :destroy, params: { id: employee.id }
+      end
+
+      it 'returns a redirect response when destroying an employee' do
+        expect(response).to have_http_status(:redirect)
+      end
+
+      it 'redirects to root path when destroying an employee' do
+        expect(response).to redirect_to(root_path)
+      end
+
+      it 'sets a flash error when destroying an employee' do
+        expect(flash[:errors]).to be_present
+      end
+    end
+
+    context 'with invalid parameters' do
+      # Add tests for invalid parameters if needed
     end
   end
 end
